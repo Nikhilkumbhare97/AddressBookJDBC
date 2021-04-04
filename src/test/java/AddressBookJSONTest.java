ae1bd642ef8fdc76acae08fdf7ced7cd0b6bf9cb
nikhilkumbhare <nikhilk190697@gmail.com>
2021-04-04 22:17:08 +0530

    Added: Ability To Add Contact Data In Json-server

--- src/test/java/AddressBookJSONTest.java
+++ src/test/java/AddressBookJSONTest.java
@@ -1,5 +1,7 @@
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -16,5 +18,17 @@ public class AddressBookJSONTest {
         Response response = RestAssured.get("http://localhost:4000/addressbook/list");
         System.out.println(response.asString());
         Assertions.assertEquals(200, response.getStatusCode());
+    }
+
+    @Test
+    public void testToContactDataInJSONServer(){
+        Response response = RestAssured.given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body("{\"firstName\": \"Anisha\",\"lastName\": \"Meshram\",\"address\": \"Hudkeshwar\",\"city\": \"Nagpur\",\"state\": \"Maharashtra\",\"zip\": \"440034\",\"phoneNumber\": \"8877445588\",\"email\": \"anisha@gmail.com\",\"personType\": \"Friend\"}")
+                .when().post("http://localhost:4000/addressbook/create");
+        response.then()
+                .body("firstName", Matchers.is("Anisha"));
+        Assertions.assertEquals(201, response.getStatusCode());
     }
 }
